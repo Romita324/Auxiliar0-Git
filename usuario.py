687e7910a603b274595954db22e21ad8a0ba9929
Samuel Andrés Romero Molina <sromeromolina10@gmail.com>
2025-03-19 18:57:56 -0300

    Modificacion metodo listarTareas

--- usuario.py
+++ usuario.py
@@ -9,14 +9,9 @@ class Usuario:
         self.tareas.append(tarea)
 
 def listarTareas(self):
-<<<<<<< HEAD
    for tarea in self.tareas:
        if tarea.estaLista():
            print(f"La tarea {tarea.obtenerNombre()} está lista")
        else: 
            print(f"La tarea {tarea.obtenerNombre()} no está lista")
-=======
-    for tarea in self.tareas:
-        if tarea.estaLista():
-            print(f"[X] {tarea.obtenerNombre()}" )
->>>>>>> ca0ec4385cc30d5e0dc544d6cd3db50177e080eb
+
